877de6502b16c8763104438910f685acdac92a15
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-06-04 23:09:06 +0530

    learning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,9 @@ import "./globals.css";
 
 export default function Home() {
   return (
-   <div className="max-w-sm mx-auto bg-white shadow-lg rounded-xl p-4">
-  <h2 className="text-xl font-semibold">Card Title</h2>
-  <p className="text-gray-600">This is a card with Tailwind classNames.</p>
-  <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-green-600">
-    Click Me
-  </button>
-</div>
+   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
+    <h2 className="text-2xl text-center text-white">Hlow</h2>
+   </main>
 
   )
 }
