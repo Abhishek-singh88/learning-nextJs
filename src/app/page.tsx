36c2d051aa8177b1f7b93eb439ab15c9687f6401
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-06-06 22:57:09 +0530

    Day 4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import "./globals.css";
 import HeroSection from "./components/HeroSection";
+import FeaturedCourse from "./components/featuredCourse";
 
 export default function Home() {
   return (
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
     <HeroSection/>
+    <FeaturedCourse/>
    </main>
 
   )
