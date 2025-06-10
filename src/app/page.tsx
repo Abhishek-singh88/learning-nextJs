678993cbd45446230e9256933375b0ce3cd7a488
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-06-10 22:59:01 +0530

    adding ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import "./globals.css";
 import HeroSection from "./components/HeroSection";
 import FeaturedCourse from "./components/featuredCourse";
+import Scroll from "./components/scroll";
+import InfiniteMovingCardsDemo from "./components/movingCard";
 
 export default function Home() {
   return (
    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
     <HeroSection/>
     <FeaturedCourse/>
+    <Scroll/>
+    <InfiniteMovingCardsDemo/>
    </main>
 
   )
