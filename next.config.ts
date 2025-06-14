473766604027239adc1dcee08764d17a2df75f0d
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-06-14 22:41:45 +0530

    disable turbo pack

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    // @ts-ignore - Turbopack is experimental and not in official types
+    turbopack: false,
+  },
 };
 
 export default nextConfig;
