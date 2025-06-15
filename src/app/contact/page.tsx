e7164f3098be75843003e4490132ebda3dac422b
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-06-15 22:53:22 +0530

    first commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+function Page() {
+  return (
+    <div className="bg-black h-241">
+      <h1 className = "text-white items-center font-bold py-100 px-90">Contact Us</h1>
+    </div>
+  );
+}
+export default Page;
