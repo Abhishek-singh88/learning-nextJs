235e40aacbc9d32f243e61b2f81b61b8f7135c8f
Abhishek Singh <abhisheksingh4928@gmail.com>
2025-06-16 18:24:52 +0530

    fixing

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,6 +2,7 @@ function Page() {
   return (
     <div className="bg-black h-241">
       <h1 className = "text-white items-center font-bold py-100 px-90">Contact Us</h1>
+      <p className="text-white items-center font-bold py-100 px-90">Email:abc@gmail.com</p>
     </div>
   );
 }
